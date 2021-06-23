8d3bf22978b888d4fb69702b1176861444d3231f
Gary Darnell <gdarnell9@gmail.com>
2021-06-23 21:27:03 +0100

    Added song snippet to chart.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -6,6 +6,9 @@ const ListItem = ({song}) => {
             <h1>{song['im:name'].label}</h1>
             <h4>By: {song['im:artist'].label}</h4>
             <img src={song['im:image'][2].label}></img>
+            <audio controls>
+                <source src={song.link[1].attributes.href}></source>
+            </audio>
         </li>
     )
 }
